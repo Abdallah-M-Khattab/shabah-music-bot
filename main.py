650f2b4b049b479ad2b98ae17c5394e739da9cdd
Abdallah-M-Khattab <abdallah.mahmoud.khattab@gmail.com>
2025-09-22 23:24:58 +0300

    main.py

--- main.py
+++ main.py
@@ -9,17 +9,21 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='-', intents=intents)
 
 # إعدادات الصوت
+# إعدادات yt-dlp المعدلة
 ytdl_format_options = {
     'format': 'bestaudio/best',
     'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
     'restrictfilenames': True,
     'noplaylist': True,
-    'nocheckcertificate': True,
+    'nocheckcertificate': True,  # ✅ أهم إضافة
     'ignoreerrors': False,
     'logtostderr': False,
     'quiet': True,
     'no_warnings': True,
     'default_search': 'auto',
+    'source_address': '0.0.0.0',
+    'extract_flat': False,
+    'verbose': False,
 }
 
 ffmpeg_options = {
@@ -145,4 +149,5 @@ async def ping(ctx):
 
 if __name__ == "__main__":
     TOKEN = os.environ['TOKEN']
+
     bot.run(TOKEN)
